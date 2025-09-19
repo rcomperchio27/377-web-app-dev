4395cbec4f154eb9274f8c5de8d5da93399e5609
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2025-09-19 12:46:47 -0400

    Python Day 1

--- AOC 2022/day1.py
+++ AOC 2022/day1.py
@@ -1,4 +1,51 @@
-file = open("day1-test.txt", "r")
+file = open('day1-data.txt', 'r')
 lines = file.readlines()
-for line in lines:
-    print(line)
+
+def part1():
+        
+    max = 0
+    max2 = 0
+    max3 = 0
+    current = 0
+
+    for line in lines:
+        line = line.strip()
+        if line == '':
+            if current > max:
+                max = current
+            current = 0
+        else:
+            current += int(line)
+
+    print(max)
+
+def part2():
+    
+    max = 0
+    max2 = 0
+    max3 = 0
+    current = 0
+
+    for line in lines:
+        line = line.strip()
+        if line == '':
+            if current > max:
+                temp = max2
+                max2 = max
+                max = current
+                max3 = temp
+            else:
+                if current > max2:
+                    max3 = max2
+                    max2 = current
+                else:
+                    if current > max3:
+                        max3 = current
+            current = 0
+        else:
+            current += int(line)
+
+    print(max + max2 + max3)
+
+
+part2()
