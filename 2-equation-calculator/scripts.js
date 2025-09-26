06ce6a26dc1d5cd7a82cd2319c8c321a7cbba408
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2025-09-26 12:45:01 -0400

    RMS ERROR

--- 2-equation-calculator/scripts.js
+++ 2-equation-calculator/scripts.js
@@ -24,9 +24,28 @@ function calculateRmsError() {
     let input = $('#rmsErrorMeasurements').val().split(/\s*,\s*/);
     let measurments = [];
 
+    min = input[0];
+
+    for (i = 0; i < input.length; i++) {
+        sigfig = input[i].length;
+
+        if (input[i].indexOf('.') != -1) {
+            sigfig = input[i].length - 1;
+        }
+        if (sigfig < min) {
+            min = sigfig;
+        }
+    }
+
+    sigfig = parseFloat(min);
+    console.log(sigfig);
+
+    // decimals = 
+
     for (i = 0; i < input.length; i++) {
         measurments[i] = parseFloat(input[i]);
     }
+
     let measurmentssum = 0; 
     for (i = 0; i < measurments.length; i++) {
         measurmentssum += measurments[i];
@@ -34,9 +53,23 @@ function calculateRmsError() {
 
     barmeasurments = measurmentssum / (measurments.length);
 
-    
+    let xiBarxSq = 0;
 
+    for (i = 0; i < measurments.length; i++) {
+        xiBarx = Math.abs(measurments[i] - barmeasurments);
+        xiBarx = Math.pow(xiBarx, 2);
+        xiBarxSq += xiBarx;
+    }
+    
+    
+    
+    let error = Math.sqrt(xiBarxSq / (measurments.length * (measurments.length - 1)));
+    error = Math.round((error + Number.EPSILON) * sigfig) / sigfig
+    console.log(error);
     console.log(measurments);
     console.log(barmeasurments);
 
+    $('#RmsResult').html(barmeasurments + '&plusmn;' + error);
 }
+
+// testdata 54.84, 53.92, 54.46, 54.55, 54.30
