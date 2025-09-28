7c99a6558bd1e4308b3640324ddacff36a211b73
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2025-09-28 17:04:14 -0400

    Final equatoiom

--- 2-equation-calculator/scripts.js
+++ 2-equation-calculator/scripts.js
@@ -3,44 +3,60 @@ function calculateDensity() {
     let length = $('#densityLength').val();
     let volume = (length * length * length);
     let density = (mass / volume);
-    $('#densityResult').html(density + 'kg/m&sup3;');
+    $('#densityResult').html(" The cube's density is: " + density + 'kg/m&sup3;');
 }
 
 function calculateVelocity() {
     let time = $('#velocityTime').val();
     let length = $('#velocityLength').val();
     let velocity = (length / time);
-    $('#velocityResult').html(velocity + 'm/s');
+    $('#velocityResult').html(' The speed is: ' + velocity + 'm/s');
 }
 
 function calculateAcceleration() {
     let time = $('#accelerationTime').val();
     let length = $('#accelerationLength').val();
     let acceleration = (length / (time * time));
-    $('#accelerationResult').html(acceleration + 'm/s&sup2;');
+    $('#accelerationResult').html(' The acceleration is: ' + acceleration + 'm/s&sup2;');
 }
 
 function calculateRmsError() {
     let input = $('#rmsErrorMeasurements').val().split(/\s*,\s*/);
     let measurments = [];
 
-    min = input[0];
+    // sigfigmin = input[0];
+    // decimin = 101 // only works up to 100 decimals
 
-    for (i = 0; i < input.length; i++) {
-        sigfig = input[i].length;
+    // for (i = 0; i < input.length; i++) {
+    //     sigfig = input[i].length;
 
-        if (input[i].indexOf('.') != -1) {
-            sigfig = input[i].length - 1;
-        }
-        if (sigfig < min) {
-            min = sigfig;
-        }
-    }
+    //     if (input[i].indexOf('.') != -1) {
+    //         sigfig = input[i].length - 1;
+    //         let num = input[i].split('.');
+    //         console.log(num);
 
-    sigfig = parseFloat(min);
-    console.log(sigfig);
+    //         if (parseInt(num[0]) > 0) {
+    //             if (num[1].length < decimin) {
+    //                 decimin = num[1].length;
+    //             }
+    //         }
+            
+
+    //     } else {
+    //         decimin = 0;
+    //     }
+    //     if (sigfig < sigfigmin) {
+    //         sigfigmin = sigfig;
+    //     }
+    // }
+
+    // sigfig = parseFloat(sigfigmin);
+    // console.log('sigfig:' + sigfig);
+    // console.log(decimin);
 
     // decimals = 
+    let sigfig = parseFloat($('#sigfig').val()) + 1;
+    let decimal = 1 * (parseFloat($('#decimal').val()) + 1); 
 
     for (i = 0; i < input.length; i++) {
         measurments[i] = parseFloat(input[i]);
@@ -51,7 +67,7 @@ function calculateRmsError() {
         measurmentssum += measurments[i];
     }
 
-    barmeasurments = Math.round((measurmentssum / (measurments.length)) * (sigfig + 1)) / (sigfig + 1);
+    barmeasurments = Math.round((measurmentssum / (measurments.length)) * (10 ** sigfig), sigfig) / (10 ** sigfig);
 
     let xiBarxSq = 0;
 
@@ -64,12 +80,11 @@ function calculateRmsError() {
     
     
     let error = Math.sqrt(xiBarxSq / (measurments.length * (measurments.length - 1)));
-    error = Math.round((error + Number.EPSILON) * sigfig) / sigfig
-    console.log(error);
+    error = Math.round((error * (10 ** decimal)), decimal) / (10 ** decimal);
     console.log(measurments);
     console.log(barmeasurments);
 
-    $('#RmsResult').html(barmeasurments + '&plusmn;' + error);
+    $('#RmsResult').html(' Your result is: ' + barmeasurments + '&plusmn;' + error);
 }
 
 // testdata 54.84, 53.92, 54.46, 54.55, 54.30
