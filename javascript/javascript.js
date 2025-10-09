597bf8f11b138888b8f9405fa0d1e280e1196a28
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2025-10-09 12:46:49 -0400

    Finnished Craps and started cards

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -13,3 +13,35 @@ function testArray() {
 
     $('#adverage').html('Your average is: ' + average);
 }
+
+function Card(suit, rank, value) {
+    this.suit = suit;
+    this.rank = rank;
+    this.value = value;
+}
+
+
+let deck = []
+
+function shuffleDeck() {
+    let suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    for (let i = 1; i < 4; i++) {
+        for (let rank = 1; rank < 14; rank++) {
+            console.log(rank + " of " + suits[i]);
+
+            // seconds parameter should translate 1, 11, 12, 13 to ace, jack, queen, king respectively
+            let card = new Card(suits[i], rank, rank);
+            deck.push(card);
+        }
+    }
+
+    // USE A READILY AVALIABLE SHUFFLE ALGORITHM (GIVE CREDIT OT THE AUTHOR) (caps)
+
+    console.log(deck);
+
+}
+
+function dealCard() {
+    let nextCard = deck.pop();
+    $('#card').html(nextCard);
+}
