9ebfd5309da5b27f96a671e9ac86b7652535880d
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2025-10-10 12:46:41 -0400

    day3

---
+++ AOC 2022/day3.py
@@ -0,0 +1,51 @@
+file = open('day3-test.txt', 'r')
+lines = file.readlines()
+
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def part1():
+    total = 0
+
+    for line in lines:
+    # // is opperator for division to be int instead of float
+        firstHalf = line[:len(line) // 2]
+        secondHalf = line[len(line) // 2:]
+       
+        print(line)
+        for letter in firstHalf:
+            if letter in secondHalf:
+                total += int(alphabet.index(letter)) + 1
+
+                print("Found Common letter: " + letter)
+                break
+    print(total)
+
+r1 = "vJrwpWtwJgWrhcsFMMfFFhFp"
+r2 = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+r3 = "PmmdzqPrVvPwwTWBwg"
+
+
+def part2():
+    counter = 0
+    total = 0
+    for line in lines:
+        counter += 1
+        if counter % 3 == 1:
+            string1 = lines[counter - 2]
+            string2 = lines[counter - 1]
+            string3 = lines[counter]
+
+            print(lines[counter])
+            for letter in string1:
+            # // is opperator for division to be int instead of float
+                for letter in string1:
+                    if letter in string2 and letter in string3:
+                        total += int(alphabet.index(letter)) + 1
+
+                        print("Found Common letter: " + letter)
+                        break
+    print(total)
+
+
+
+part2()
