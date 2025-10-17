cf295cc0ebc35f45fabb6c3969af45c00828043f
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2025-10-16 22:14:43 -0400

    first two numebrs in grid

--- 3-svg-game/scripts.js
+++ 3-svg-game/scripts.js
@@ -1,26 +1,45 @@
-function loadpuzzle() {
+let grid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0
+];
+
+function createPuzzle() {
+    let numpicked = true;
+    grid[0] = Math.ceil(Math.random(0, 1) * 9);
+    currentnum = grid[0]
+    while (numpicked) {
+        let num = Math.ceil(Math.random(0, 1) * 9);
+        for (i = 1; i < 9; i++) {
+            if (num != currentnum) {
+                numpicked = false;
+                grid[1] = num;
+            }
+        }
+    }
+    console.log(grid);
+}
+
+function loadPuzzle() {
     let side = 64;
     let padding = 3;
-    let border = 5
+    let border = 5;
 
     for (y = 0; y < 9 * side; y += side) {
         for (x = 0; x < 9 * side; x += side) {
             let square = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-            if (y % 3 == 0) {
-                square.setAttribute('y', y + ((padding) * (y / 50) + border));
-            } else {
-                square.setAttribute('y', y + (padding * (y / 50) + border));             
-            }
-            if (y % 3 == 0) {
-                square.setAttribute('x', x + ((padding) * (x / 50) + border));
-            } else {
-                square.setAttribute('x', x + (padding * (x / 50) + border));             
-            }            
+            let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            square.setAttribute('y', y + ((padding) * (y / 50) + border));
+            square.setAttribute('x', x + ((padding) * (x / 50) + border));
             square.setAttribute('width', side);
             square.setAttribute('height', side);
             square.setAttribute('class', 'tile');
-            square.setAttribute('fill', 'white');
-
+            square.setAttribute('id', ((y / side) * 9) + (x / side));
             svg.appendChild(square);
         }
     }
