f32b7838ad9d1db2de00d6346cf3608e729dabd7
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2025-10-19 21:36:25 -0400

    Made grid better

--- 3-svg-game/scripts.js
+++ 3-svg-game/scripts.js
@@ -9,47 +9,57 @@ let grid = [0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0, 0
 ];
 
-function createPuzzle() {
-    let numpicked = true;
-    grid[0] = Math.ceil(Math.random(0, 1) * 9);
-    currentnum = grid[0]
-    while (numpicked) {
-        let num = Math.ceil(Math.random(0, 1) * 9);
-        for (i = 1; i < 9; i++) {
-            if (num != currentnum) {
+function createPuzzle(currentRow) {
+    for (number = 0; number < 9; number++) {
+        let numpicked = true;
+        while (numpicked) {
+            let num = Math.ceil(Math.random(0, 1) * 9);
+            checkRow = (num != grid[(currentRow * 9) - 9] && num != grid[(currentRow * 9) - (1)] && num != grid[(currentRow * 9) - (2)] && num != grid[(currentRow * 9) - (3)] && num != grid[(currentRow * 9) - (4)]
+            && num != grid[(currentRow * 9) - (5)] && num != grid[(currentRow * 9) - (6)] && num != grid[(currentRow * 9) - (7)] && num != grid[(currentRow * 9) - (8)]);
+
+            if (checkRow) {
                 numpicked = false;
-                grid[1] = num;
-            }
+                grid[((currentRow - 1) * 9) + number] = num;
+                
+            }           
         }
+        console.log(grid)
     }
-    console.log(grid);
 }
 
 function loadPuzzle() {
+    createPuzzle(1)
     let side = 64;
     let xpadding = 3;
     let ypadding = 3;
-    let border = 5;
+    let xtextIndent = 32;
+    let ytextIndent = 40;
 
-    for (y = 0; y < 9; y ++) {
-        xpadding = 3
-        for (x = 0; x < 9; x++) {
+    for (row = 0; row < 9; row ++) {
+        if (row % 3 == 0) {
+            ypadding += 3;
+        }
+        xpadding = 3;
+        for (column = 0; column < 9; column++) {
             let square = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
             let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-            if (x % 3 == 0 ) {
-                xpadding += 3
+            if (column % 3 == 0 ) {
+                xpadding += 3;
             } 
-            if (y % 3 == 0) {
-                ypadding -= 3
+            text.setAttribute('y', (row * side) + ((3 * row) + ypadding + ytextIndent));
+            text.setAttribute('x', (column * side) + ((3 * column) + xpadding + xtextIndent));
+            text.setAttribute('class', 'text');
+            text.setAttribute('id', ('text' + (row * 9) + (column)));
 
-            }
-            square.setAttribute('y', (y * side) + ((3 * y) + border + ypadding));
-            square.setAttribute('x', (x * side) + ((3 * x) + border + xpadding));
+            square.setAttribute('y', (row * side) + ((3 * row) + ypadding));
+            square.setAttribute('x', (column * side) + ((3 * column) + xpadding));
             square.setAttribute('width', side);
             square.setAttribute('height', side);
             square.setAttribute('class', 'tile');
-            square.setAttribute('id', (y * 9) + (x));
+            square.setAttribute('id',('tile' + (row * 9) + (column)));
             svg.appendChild(square);
+            svg.appendChild(text);
+            text.innerHTML = String(grid[(row * 9) + (column)]);
         }
     }
 
