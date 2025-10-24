e5d268f3441885f666dc2a2aae4b3498dc0e2d17
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2025-10-23 20:50:13 -0400

    Finished game

--- 3-svg-game/scripts.js
+++ 3-svg-game/scripts.js
@@ -9,16 +9,42 @@ let solutions = [[2, 3, 9, 6, 1, 7, 8, 5, 4,
                 7, 8, 1, 9, 3, 4, 5, 2, 6, 
                 5, 2, 3, 7, 6, 1, 9, 4, 8, 
                 9, 4, 6, 5, 8, 2, 7, 1, 3], 
-
-                [0, 0, 0, 6, 0, 0, 8, 0, 4, 
-                6, 0, 0, 0, 0, 0, 2, 0, 0, 
-                0, 7, 8, 4, 2, 5, 6, 0, 0, 
-                3, 0, 0, 0, 7, 0, 0, 6, 0, 
-                0, 0, 5, 0, 4, 0, 1, 0, 0, 
-                0, 0, 0, 1, 5, 0, 0, 0, 0, 
-                7, 0, 0, 0, 3, 0, 0, 0, 0, 
-                0, 2, 0, 0, 0, 0, 9, 0, 0, 
-                9, 4, 0, 5, 0, 2, 0, 1, 3]]
+               [2, 9, 6, 5, 3, 8, 4, 1, 7, 
+                8, 3, 7, 9, 4, 1, 6, 2, 5, 
+                5, 1, 4, 6, 7, 2, 8, 3, 9, 
+                3, 5, 1, 4, 8, 9, 2, 7, 6, 
+                7, 4, 2, 1, 6, 3, 5, 9, 8, 
+                6, 8, 9, 2, 5, 7, 3, 4, 1, 
+                4, 2, 5, 7, 9, 6, 1, 8, 3, 
+                1, 7, 8, 3, 2, 5, 9, 6, 4, 
+                9, 6, 3, 8, 1, 4, 7, 5, 2],
+               [9, 6, 8, 7, 3, 5, 1, 4, 2, 
+                4, 5, 1, 9, 8, 2, 7, 3, 6, 
+                3, 2, 7, 1, 6, 4, 8, 5, 9, 
+                2, 3, 9, 6, 7, 1, 5, 8, 4, 
+                1, 8, 4, 2, 5, 9, 6, 7, 3, 
+                5, 7, 6, 8, 4, 3, 2, 9, 1, 
+                6, 4, 2, 5, 9, 7, 3, 1, 8, 
+                7, 1, 3, 4, 2, 8, 9, 6, 5, 
+                8, 9, 5, 3, 1, 6, 4, 2, 7],
+               [5, 4, 9, 2, 8, 6, 7, 1, 3, 
+                2, 1, 7, 4, 3, 9, 8, 6, 5, 
+                3, 8, 6, 7, 5, 1, 4, 9, 2, 
+                1, 9, 3, 5, 7, 2, 6, 4, 8, 
+                4, 7, 8, 9, 6, 3, 2, 5, 1, 
+                6, 2, 5, 8, 1, 4, 3, 7, 9, 
+                8, 5, 4, 3, 9, 7, 1, 2, 6, 
+                7, 3, 1, 6, 2, 5, 9, 8, 4, 
+                9, 6, 2, 1, 4, 8, 5, 3, 7],
+               [9, 1, 5, 8, 6, 4, 7, 3, 2, 
+                7, 6, 8, 2, 5, 3, 9, 4, 1, 
+                3, 2, 4, 1, 7, 9, 5, 6, 8, 
+                4, 9, 7, 6, 1, 2, 3, 8, 5, 
+                1, 8, 2, 5, 3, 7, 6, 9, 4, 
+                6, 5, 3, 9, 4, 8, 2, 1, 7, 
+                8, 7, 6, 3, 2, 1, 4, 5, 9, 
+                5, 4, 9, 7, 8, 6, 1, 2, 3, 
+                2, 3, 1, 4, 9, 5, 8, 7, 6]]
 
 let solution = solutions[0]
 
@@ -31,7 +57,6 @@ let grids = [['', '', '', 6, '', '', 8, '', 4,
             7, '', '', '', 3, '', '', '', '', 
             '', 2, '', '', '', '', 9, '', '', 
             9, 4, '', 5, '', 2, '', 1, 3], 
-            
            ['', 9, '', '', '', '', 4, '', '', 
             '', '', '', '', '', '', 6, 2, '', 
             5, 1, '', 6, 7, '', '', 3, '', 
@@ -40,7 +65,34 @@ let grids = [['', '', '', 6, '', '', 8, '', 4,
             '', '', '', 2, '', '', '', '', '', 
             4, '', 5, '', 9, 6, '', '', 3, 
             '', '', 8, '', '', '', '', '', '', 
-            9, 6, '', '', '', 4, 7, 5, '']]
+            9, 6, '', '', '', 4, 7, 5, ''],
+           [9, '', '', '', 3, '', '', '', '', 
+            4, '', '', '', 8, 2, '', '', '', 
+            3, '', 7, '', '', 4, 8, 5, '', 
+            '', 3, '', '', '', 1, '', '', 4, 
+            '', '', 4, 2, 5, '', '', '', 3, 
+            '', 7, '', 8, '', '', 2, '', 1, 
+            6, '', '', 5, '', '', '', 1, '', 
+            7, 1, 3, '', '', '', 9, '', '', 
+            '', 9, 5, '', '', '', 4, '', ''], 
+           [5, '', 9, 2, '', '', 7, 1, '', 
+            '', '', '', 4, '', 9, 8, '', 5, 
+            '', '', '', '', '', '', '', 9, 2, 
+            '', '', '', '', 7, 2, 6, '', '', 
+            '', 7, 8, '', 6, '', 2, '', '', 
+            6, 2, '', '', 1, 4, '', '', 9, 
+            8, 5, '', '', 9, '', '', '', '', 
+            '', '', 1, 6, '', '', '', '', 4, 
+            '', '', 2, '', '', '', 5, '', ''], 
+           ['', 1, '', 8, 6, '', '', 3, '', 
+            7, '', '', '', 5, '', '', '', '', 
+            '', '', '', '', '', '', 5, 6, 8, 
+            4, '', '', 6, 1, '', '', '', '', 
+            1, '', '', '', 3, 7, '', 9, '', 
+            '', '', '', 9, 4, '', 2, 1, 7, 
+            8, '', '', '', '', '', 4, '', 9, 
+            5, 4, '', 7, '', '', '', 2, 3, 
+            2, '', '', '', 9, '', '', '', 6]]
 
 let grid = grids[0];
 
@@ -102,25 +154,6 @@ function secondBox() {
 function createPuzzle(currentRow) {
     firstBox()
     secondBox()
-    // for (number = 0; number < 9; number++) {
-    //     let numpicked = true;
-    //     while (numpicked) {
-    //         let num = Math.ceil(Math.random(0, 1) * 9);
-    //         checkRow = (num != solution[(currentRow * 9) - 9] && num != solution[(currentRow * 9) - (1)] && num != solution[(currentRow * 9) - (2)] && num != solution[(currentRow * 9) - (3)] && num != solution[(currentRow * 9) - (4)]
-    //         && num != solution[(currentRow * 9) - (5)] && num != solution[(currentRow * 9) - (6)] && num != solution[(currentRow * 9) - (7)] && num != solution[(currentRow * 9) - (8)]);
-
-    //         if (checkRow) {
-    //             numpicked = false;
-    //             solution[((currentRow - 1) * 9) + number] = num;
-                
-    //         }           
-    //     }
-    //     console.log(solution)
-    // }
-    // for (i = 0; i < $('#remove').val(); i++) {
-    //     blanklist.push(Math.floor(Math.random(0, 1) * 81))
-    // }
-    // console.log(blanklist)
 }
 
 function lastPuzzle() {
@@ -148,8 +181,9 @@ function nextPuzzle() {
 }
 
 function loadPuzzle() {
-    // delete the previous puzzle
-    // for (i = 0, i < svg.length)
+    const svg = document.querySelector('svg');
+    svg.innerHTML = '';
+  
     let side = 64;
     let xpadding = 3;
     let ypadding = 3;
@@ -182,17 +216,6 @@ function loadPuzzle() {
             svg.appendChild(square);
             svg.appendChild(text);
 
-            // blank = false
-            // for (i = 0; i < blanklist.length; i++) {
-            //     if ((parseInt(row * 9) + parseInt(column)) == blanklist[i]) {
-            //         blank = true;
-            //     }
-            // }
-
-            // if (blank == false) {
-            //     text.innerHTML = String(grid[(parseInt(row * 9) + parseInt(column))]);
-            // }
-
             text.innerHTML = String(grid[(parseInt(row * 9) + parseInt(column))]);
 
             if ($('#text' + (parseInt(row * 9) + parseInt(column))).html() == '0' || $('#text' + (parseInt(row * 9) + parseInt(column))).html() == '') {
@@ -211,12 +234,8 @@ function loadPuzzle() {
 }
 
 function tileclicked(tileid) {
-    $('#backbutton').attr('onclick', '')
-    $('#nextbutton').attr('onclick', '')
-    console.log('a')
     textid = 'text' + tileid.substring(4);
     if ($('#' + tileid).attr('class') == 'given') {
-        console.log('b')
         return
     }
     if ($('#' + textid).html() == '') {
@@ -231,8 +250,6 @@ function tileclicked(tileid) {
 }
 
 function checkPuzzle() {
-    $('#backbutton').attr('onclick', '')
-    $('#nextbutton').attr('onclick', '')
     for (i = 0; i < 81; i++) {
         if (grid[i] == solution[i] && $('#tile' + i).attr('class') != "given") {
             $('#tile' + i).css('fill', 'lightgreen');
