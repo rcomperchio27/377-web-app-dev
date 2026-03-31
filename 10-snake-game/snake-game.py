4fcedc501bd3c7ebd5db291c9c67a3fdcd6cafdb
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2026-03-31 12:46:25 -0400

    added menu

--- 10-snake-game/snake-game.py
+++ 10-snake-game/snake-game.py
@@ -1,7 +1,9 @@
 import pygame
 import time
 import random
- 
+# Imports menu
+import pygame_menu 
+
 pygame.init()
  
 white = (255, 255, 255)
@@ -10,6 +12,9 @@ black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
+
+# Added a list of different colors for the snakes
+snakeColors = [(180, 30, 60), (0, 60, 120)]
 
 dis_width = 600
 dis_height = 400
@@ -25,16 +30,19 @@ snake_speed = 15
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
  
+# Changed the score to display both p1's score and p2's score
+def Your_score(p1_score, p2_score):
+    score_title_txt = score_font.render("Score", True, yellow)
+    score_txt = score_font.render(str(p1_score) + "|" + str(p2_score), True, yellow)
+
+    dis.blit(score_title_txt, [(dis_width / 2) - (score_title_txt.get_width() / 2), 0])
+    dis.blit(score_txt, [(dis_width / 2) - (score_txt.get_width() / 2), score_title_txt.get_height() - 10])
  
-def Your_score(score):
-    value = score_font.render("Your Score: " + str(score), True, yellow)
-    dis.blit(value, [0, 0])
  
- 
- 
-def our_snake(snake_block, snake_list):
+# Added snakenum to differenciate between the snakes and thier colors 
+def our_snake(snake_block, snake_list, snakenum):
     for x in snake_list:
-        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+        pygame.draw.rect(dis, snakeColors[snakenum], [x[0], x[1], snake_block, snake_block])
  
  
 def message(msg, color):
@@ -46,17 +54,17 @@ def gameLoop():
     game_over = False
     game_close = False
  
-    snake1_x1 = (dis_width * 3) / 4 
+    snake1_x1 = (dis_width * 1) / 4 
     snake1_y1 = dis_height / 2
  
     snake1_x1_change = 0
     snake1_y1_change = 0
  
     snake_List = []
-    Length_of_snake = 1
+    Length_of_snake1 = 1
 
     # Snake 2 variables
-    snake2_x1 = dis_width / 4 
+    snake2_x1 = (dis_width * 3) / 4 
     snake2_y1 = dis_height / 2
  
     snake2_x1_change = 0
@@ -74,7 +82,7 @@ def gameLoop():
         while game_close == True:
             dis.fill(blue)
             message("You Lost! Press C-Play Again or Q-Quit", red)
-            Your_score(Length_of_snake - 1)
+            Your_score(Length_of_snake1 - 1, Length_of_snake2 - 1)
             pygame.display.update()
  
             for event in pygame.event.get():
@@ -137,7 +145,10 @@ def gameLoop():
         snake1_x1 += snake1_x1_change
         snake1_y1 += snake1_y1_change
 
-        # Snake 2 changes in direction
+        # Snake 2 changes in direction and checks if its off the window
+        if snake2_x1 >= dis_width or snake2_x1 < 0 or snake2_y1 >= dis_height or snake2_y1 < 0:
+            game_close = True
+
         snake2_x1 += snake2_x1_change
         snake2_y1 += snake2_y1_change
 
@@ -166,7 +177,7 @@ def gameLoop():
                 game_close = True
 
 
-        if len(snake_List) > Length_of_snake:
+        if len(snake_List) > Length_of_snake1:
             del snake_List[0]
 
         for x in snake_List[:-1]:
@@ -181,20 +192,20 @@ def gameLoop():
             if x == snake2_Head:
                 game_close = True
         
-
-        our_snake(snake_block, snake_List)
+        # added number for snake color
+        our_snake(snake_block, snake_List, 0)
 
         # Draws the second snake
-        our_snake(snake_block, snake2_List)
+        our_snake(snake_block, snake2_List, 1)
 
-        Your_score(Length_of_snake - 1)
+        Your_score(Length_of_snake1 - 1, Length_of_snake2 - 1)
  
         pygame.display.update()
  
         if snake1_x1 == foodx and snake1_y1 == foody:
             foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
             foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-            Length_of_snake += 1
+            Length_of_snake1 += 1
  
         # Snake 2 eat food
         if snake2_x1 == foodx and snake2_y1 == foody:
@@ -207,5 +218,13 @@ def gameLoop():
     pygame.quit()
     quit()
  
- 
-gameLoop()
+# Add menu
+menu = pygame_menu.Menu('Welcome', 400, 300,
+                       theme=pygame_menu.themes.THEME_BLUE)
+
+# menu.add.text_input('Name :', default='John Doe')
+# menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu.add.button('Play', gameLoop)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
+menu.mainloop(dis)
