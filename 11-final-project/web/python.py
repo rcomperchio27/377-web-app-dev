72432e8c2ea64be42093367e219573810645a683
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2026-04-14 12:46:27 -0400

    trying to import svg

--- 11-final-project/web/python.py
+++ 11-final-project/web/python.py
@@ -1,11 +1,15 @@
 # run pip install chess
+# pip install brython
 
 from browser import document, html
 
 import chess
+import xml.etree.ElementTree as ET
+import chess.svg
 
 board = chess.Board()
 
 print(board)
+print(chess.svg.piece(chess.Piece.from_symbol("R")))
 
-document = board
+document = chess.Piece.from_symbol("R")
