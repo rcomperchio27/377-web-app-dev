c5b3280dedc523ad9b047dbb93c09c876355e950
rcomperchio27 <rcomperchio27@hanoverstudents.org>
2026-04-27 12:47:27 -0400

    install djnago

--- 11-final-project/web/python.py
+++ 11-final-project/web/python.py
@@ -1,8 +1,10 @@
 # run pip install chess
 # pip install brython
+# python -m pip install Django==6.0.4
 
 from browser import document, html, svg
 
+import django
 import chess
 import chess.svg
 
@@ -10,8 +12,7 @@ board = chess.Board()
 
 print(str(board))
 
-# piece = chess.svg.piece(chess.Piece.from_symbol("R"))
 piece = chess.svg.PIECES["R"]
 print(piece)
 
-document <= "<svg>" + str(piece) + "</svg>"
+document["BoardSVG"].attach(html.SVG(str(chess.svg.PIECES["R"]), xmlns="http://www.w3.org/2000/svg"), width=45, height=45)
